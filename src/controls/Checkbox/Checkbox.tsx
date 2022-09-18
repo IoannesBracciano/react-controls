06b58e80857e388f36d76fcfabfffe542cf07cf0
Ioannes Bracciano <john.bracciano@hotmail.gr>
2022-09-18 17:40:01 +0200

    Slightly Refactor & Write Documentation For Checkbox

--- src/controls/Checkbox/Checkbox.tsx
+++ src/controls/Checkbox/Checkbox.tsx
@@ -1,14 +1,6 @@
 import React, { useCallback, useState } from 'react'
 import styled, { css } from 'styled-components'
-import {
-  brightness,
-  container,
-  focusoutline,
-  raise,
-  rect,
-  solid,
-  ThemeResolver
-} from '../../theming'
+import { brightness, container, focusoutline, rect, solid } from '../../theming'
 
 const StyledInput = styled('input')<
   Omit<CheckboxProps, 'size'> & { scale: 'sm' | 'md' | 'lg' | undefined }
@@ -19,74 +11,80 @@ const StyledInput = styled('input')<
       scale
     })}
   ${focusoutline(':after')}
-  ${brightness('107%', [':focus', ':hover'])}
+  ${brightness('103%', [':focus', ':hover'])}
   ${brightness('97%', [':active'])}
-  ${raise(3, [':focus:after', ':hover:after'])}
   -webkit-tap-highlight-color: transparent;
   appearance: none;
   cursor: pointer;
   font-family: 'Lato', sans-serif;
   transition: all 0.15s;
-  ${({ accent }) => css`
-    &:after {
-      ${solid('#fafafa')}
-      ${rect({
-        height: '1em',
-        radiusIndex: 2,
-        width: '1em'
-      })}
-      border-color: ${ThemeResolver.getAccentColor(accent)}80;
-      content: '';
-      display: block;
-      transition: all 0.15s;
-    }
+  ${({ theme }) => {
+    const grayLight = theme.palette.gray['30']
+    const grayLighter = theme.palette.gray['5']
+    return css`
+      &:after {
+        ${solid(grayLighter)}
+        ${rect({
+          height: '1em',
+          radiusIndex: 1,
+          width: '1em'
+        })}
+        border-color: ${grayLight};
+        content: '';
+        display: block;
+        transition: all 0.15s;
+      }
 
-    &:before {
-      ${rect({
-        height: '1em',
-        radiusIndex: 2,
-        width: '1em'
-      })}
-      content: '✔';
-      display: block;
-      opacity: 0;
-      position: absolute;
-      z-index: 9;
-      line-height: 1.5em;
-      font-size: 0.7em;
-      text-align: center;
-      width: 100%;
-      height: 100%;
-      transition: all 0.15s;
-      color: #fff;
-    }
-  `}
+      &:before {
+        content: '✔';
+        display: block;
+        position: absolute;
+        z-index: 9;
+        line-height: 1.5em;
+        font-size: 0.7em;
+        text-align: center;
+        width: 100%;
+        height: 100%;
+        transition: all 0.15s;
+        color: #fff;
+      }
+    `
+  }}
 
-  &:checked:after {
-    ${({ accent }) => solid(accent)};
-    border-color: transparent;
-  }
-
-  &:focus::before,
-  &:hover:before {
-    box-shadow: 0 0 0.2em 0 #00000020;
-  }
-
-  ${({ accent }) => css`
-    &:checked:before {
-      opacity: 1;
-      color: ${ThemeResolver.getInkForAccentedSurface(accent)};
-    }
-  `}
+  ${({ accent, theme }) => {
+    const bg = theme.background.darker[accent]
+    return css`
+      &:checked:after {
+        ${solid(bg)}
+      }
+    `
+  }}
 `
 
 export interface CheckboxProps {
+  /**
+   * Choose the color variation of the control.
+   */
   accent: 'error' | 'primary' | 'secondary' | 'success' | 'warning'
+  /**
+   * Set the initial checked state of the checkbox.
+   */
   checked?: boolean
+  /**
+   * Get notified whenever the checkbox's state is changed.
+   */
   onToggle?: (isOn: boolean) => void
+  /**
+   * Choose from 3 standard control sizes calculated based on the
+   * applied theme's base font size.
+   */
   size?: 'sm' | 'md' | 'lg'
 }
 
+/**
+ * Checkboxes inform users of their options and allow them to easily
+ * opt in or pass on specific items.
+ */
 export const Checkbox = ({
   accent = 'primary',
   checked = false,
